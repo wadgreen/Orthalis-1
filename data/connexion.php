69b04917b0479ee728e7ad462d56ca35020cce2c
mIaborde <mlaborde@fabriquenumerique.fr>
2018-07-13 15:54:47 +0200

    Formulaire -> Ajax -> connexion.php -> Ajax -> localStorage -> page suivante = fonctionnel

--- data/connexion.php
+++ data/connexion.php
@@ -2,8 +2,6 @@
 
 header("Access-Control-Allow-Origin: *");
 
-@ini_set('display_errors', 'on'); 
-
 // VARIABLES DE CONNEXION
 $DB_host = "mysql458.sql004";
 $DB_user = "fabriquetssimplo";
@@ -11,24 +9,25 @@ $DB_name = "fabriquetssimplo";
 $DB_pass = "qgtsNaJy4FHh";
 
 $code_cabinet = $_POST['codeCabinet'];
-//var_dump($_POST['codeCabinet']);
 
 try
 {
-    // ON SE CONNECTE ET ON SELECTIONNE LA BASE
+    // On se connecte et on selectionne la base
     $DB_con = new PDO("mysql:host={$DB_host};dbname={$DB_name}",$DB_user,$DB_pass);
     $DB_con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    $query = $DB_con->prepare("SELECT code_cabinet FROM cabinet WHERE code_cabinet = :codeCabinet");
+    // On prépare la requête SQL
+    $query = $DB_con->prepare("SELECT id_cabinet FROM cabinet WHERE code_cabinet = :codeCabinet");
     $query->bindParam('codeCabinet', $code_cabinet);
-        
     $query->execute();
-    $result = $query->fetch();
-    if($result[0] != FALSE){
-        //Si oui nous accueillons l'utilisateur identifié
-        echo "Success";  
+    // On récupère le résultat de la requête sous forme de tableau associatif
+    $result = $query->fetchAll();
+
+    // On teste si le tableau associatif est vide
+    if(!empty($result)){
+        // Si non on retourne l'id du cabinet
+        echo $result[0]['id_cabinet'];  
     }else{
-        //Sinon nous signalons une erreur d'identifiant ou de mot de passe
+        // Si oui il y'a erreur
         echo "Error";
     }
 }
